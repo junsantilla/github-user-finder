e34c881f29aabb57bb6b0a5a901d5948f05e1f71
junsantilla <junsantilla@live.com>
2022-04-14 15:23:12 +0800

    Remove token

--- github.js
+++ github.js
@@ -1,30 +1,18 @@
 class Github {
 	constructor() {
-		this.client_id = "f6a81fb190d5b104f236";
-		this.client_secret = "06d002df18546977433795dadcf7eb447a3c2027";
+		this.client_id = "e716a832d86919e8ce8b";
+		this.client_secret = "26d52a97539c960a7e585eef3c83ceb8c7b863b3";
 		this.repos_count = 5;
 		this.repos_sort = "created: asc";
 	}
 
 	async getUser(user) {
 		const profileResponse = await fetch(
-			`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`,
-			{
-				headers: {
-					authorization:
-						"token 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",
-				},
-			}
+			`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
 		);
 
 		const repoResponse = await fetch(
-			`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`,
-			{
-				headers: {
-					authorization:
-						"token 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",
-				},
-			}
+			`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
 		);
 
 		const profile = await profileResponse.json();
